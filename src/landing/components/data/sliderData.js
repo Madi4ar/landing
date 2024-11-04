520523bb3bac3820a2fdc213b2aa8a11944e228d
Madiyar Talgatuly <133092635+Madi4ar@users.noreply.github.com>
2024-11-04 16:33:46 +0500

    Update sliderData.js

--- src/landing/components/data/sliderData.js
+++ src/landing/components/data/sliderData.js
@@ -1,22 +1,22 @@
 const sliderData = [
   {
-    author: 'Bret Taylor',
-    company: 'Retired President & COO, Salesforce',
+    author: 'Nurlan Smagulov',
+    company: 'Mega Center',
     text: `"Thanks to companies like bizdin ai, a lot of these SaaS tools are building intelligent capabilities into their products[...] that's a path for a lot of companies to get value from Al without becoming Al shops, which is a really formidable challenge."`,
   },
   {
-    author: 'Bret Taylor',
-    company: 'Retired President & COO, Salesforce',
+    author: 'Nurlan Smagulov',
+    company: 'Mega Center',
     text: `"Thanks to companies like bizdin ai, a lot of these SaaS tools are building intelligent capabilities into their products[...] that's a path for a lot of companies to get value from Al without becoming Al shops, which is a really formidable challenge."`,
   },
   {
-    author: 'Bret Taylor',
-    company: 'Retired President & COO, Salesforce',
+    author: 'Nurlan Smagulov',
+    company: 'Mega Center',
     text: `"Thanks to companies like bizdin ai, a lot of these SaaS tools are building intelligent capabilities into their products[...] that's a path for a lot of companies to get value from Al without becoming Al shops, which is a really formidable challenge."`,
   },
   {
-    author: 'Bret Taylor',
-    company: 'Retired President & COO, Salesforce',
+    author: 'Nurlan Smagulov',
+    company: 'Mega Center',
     text: `"Thanks to companies like bizdin ai, a lot of these SaaS tools are building intelligent capabilities into their products[...] that's a path for a lot of companies to get value from Al without becoming Al shops, which is a really formidable challenge."`,
   },
 ];
